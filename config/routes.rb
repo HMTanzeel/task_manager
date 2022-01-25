572216e72fa541f48d99422b1895bad586498729
HMTanzeel <mtanzeel6363@gmail.com>
2022-01-25 19:12:56 +0500

    change

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,4 @@ Rails.application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   
-    # get 'signup', to: 'users#new', as: 'signup'
-    # get 'login', to: 'sessions#new', as: 'login'
-    # delete 'logout', to: 'sessions#destroy'
-  
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
