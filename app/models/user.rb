66dac5121a4b20197d0f5e9e8a3e7d84c20a0066
HMTanzeel <mtanzeel6363@gmail.com>
2022-02-10 17:28:48 +0500

    tasks model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :messages, dependent: :destroy
+  has_many :tasks
   has_secure_password
   validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_130815) do
+ActiveRecord::Schema.define(version: 2022_02_10_060921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2022_01_19_130815) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "discription"
+    t.datetime "started_at", precision: 6
+    t.string "close_at"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_01_19_130815) do
   end
 
   add_foreign_key "messages", "users"
+  add_foreign_key "tasks", "users"
 end
