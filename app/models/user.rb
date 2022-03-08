32a49a48c2d335fa05ce46996078f37619daf4e2
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-08 18:07:52 +0500

    Create project model with all it's associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :notes, dependent: :destroy
   has_many :tasks
+  has_many :projects
   has_secure_password
   validates :email, presence: true, uniqueness: true
   enum :role, [:admin, :employee]
