c8dd03c5e2ae79b2d1545c8fac2d1d5036dc5d6f
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-10 19:37:56 +0500

    test cases for project model

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,20 +1,44 @@
 require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
+  test "valid with user" do
+    user = User.create(name: "Ali", mobile: "0900", email: "a2@g.com", password_digest: "123", address: "ajhsncjfacnaje")
+    assert user.valid?
+
+    assert_empty user.errors
+  end
+
+  test "invalid without user" do
+    user = User.create()
+    assert user.valid?
+
+    assert_empty user.errors
+  end
+
   test "valid with user and if title is not empty" do
+    user = User.create(name: "Ali", mobile: "0900", email: "a2@g.com", password_digest: "123", address: "ajhsncjfacnaje")
     project = Project.new(title: "any")
+    project.user = user
+    assert project.valid?
+
     assert_empty project.errors
   end
-  
-  # test "valid if active is equal to true or false" do
-  #   project = Project.new(active: false)
-  #   project.valid?
-  #   assert_not project.errors[:active].empty?
-  # end
 
-  # test "invalid if project_type is greater than 1" do
-  #   project = Project.new(project_type: 0)
-  #   project.valid?
-  #   assert_empty project.errors[:project_type].empty?
-  # end
+  test "invalid with user and if title is empty" do
+    user = User.create(name: "Ali", mobile: "0900", email: "a2@g.com", password_digest: "123", address: "ajhsncjfacnaje")
+    project = Project.new
+    project.user = user
+    assert project.valid?
+
+    assert_empty project.errors
+  end
+
+  test "invalid if title and user is not available" do
+    user = User.create
+    project = Project.new
+    project.user = user
+    assert project.valid?
+
+    assert_empty project.errors
+  end
 end
