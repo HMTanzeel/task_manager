e87459eb24a37533b5ed9b953c29345497b1d87e
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-15 12:26:45 +0500

    remove space add bootstrap on select tag and set dependent destroy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  has_many :tasks
+  belongs_to :user
+  has_many :tasks, dependent: :destroy
   validates :title, presence: true
   enum project_type: [:one_time, :support]
 end
