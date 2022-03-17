3093dcb879af132f3739357d220c0542506acc2b
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-17 16:32:57 +0500

    user fixtures test cases and install factory bot gem

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,19 +1,17 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  fixtures :users
+  
   test "check user one existance" do
-    assert users(:one)
+    pass=[id: users(:one).id, name: users(:one).name, email: users(:one).email, password_digest: users(:one).password_digest, address: users(:one).address]
+    user = User.create(pass)
+    assert user.present?
   end
 
-  test "check user two existance by ID" do
-    assert users(:two).id
-  end
-
-  test "find password of the user one" do
-    assert_equal "12345", users(:one).password_digest
-  end
-
-  test "find password of the given user two" do
-    assert_equal "Always", users(:two).password_digest
+  test "check user two existance" do
+    pass=[id: users(:two).id, name: users(:two).name, email: users(:two).email, password_digest: users(:two).password_digest, address: users(:two).address]
+    user = User.create(pass)
+    assert user.present?
   end
 end
