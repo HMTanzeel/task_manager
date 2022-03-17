e8790ea30422a9219ae24f97e0075dd1938e88f5
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-17 14:00:25 +0500

    user fixtures test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "check user one existance" do
+    assert users(:one)
+  end
+
+  test "check user two existance by ID" do
+    assert users(:two).id
+  end
+
+  test "find password of the user one" do
+    assert_equal "12345", users(:one).password_digest
+  end
+
+  test "find password of the given user two" do
+    assert_equal "Always", users(:two).password_digest
+  end
 end
