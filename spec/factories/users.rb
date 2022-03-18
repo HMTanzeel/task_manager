4098d3923a2530d4de58c758b87de269eb00ac7a
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-18 12:47:59 +0500

    rspec case for user model with factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'Tanzeel' }
+    mobile { '1234' }
+    email { 'a1@g.com' }
+    password_digest { '12345' }
+    address { 'qwertyuiop' }
   end
 end
