ff0b6b5891dbae48f8272ef874b87827bdca446e
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-18 15:50:22 +0500

    rspec case for user model with factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "must create a user via factory bot data" do
+  it "create a user" do
     user1 = FactoryBot.create(:user)
     expect(user1).to be_valid
+  end
+
+  it "valid if name is equal" do
+    user1 = FactoryBot.create(:user)
+    expect(user1.name).to eq('Tanzeel')
   end
 end
