d40e67fb898d9afd8015dc835b21c274eda7470a
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-22 12:46:03 +0500

    project_crud reviw's feedback

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get projects_index_url
+    user = User.create(address: 'Lahore', email: "git@g.com", mobile: '0300', name: "john", password: "secret", password_confirmation: "secret")
+    get user_projects_path(user.id)
     assert_response :success
   end
 end
