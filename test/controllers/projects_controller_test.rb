3f04b7f1b9db761c0c5e99b662cec1c05ca705c7
HMTanzeel <mtanzeel6363@gmail.com>
2022-03-24 17:58:49 +0500

    PR feedback of client crud

--- test/controllers/projects_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    user = User.create(address: 'Lahore', email: "git@g.com", mobile: '0300', name: "john", password: "secret", password_confirmation: "secret")
-    get user_projects_path(user.id)
-    assert_response :success
-  end
-end
